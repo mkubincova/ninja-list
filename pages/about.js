6c130b642851554c9d7fdb2f104b2b38035621ab
magda <magdalena.kubincova@protonmail.com>
2022-05-23 19:15:55 +0200

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+const About = () => {
+    return ( 
+        <>
+            <Head>
+                <title>Ninja list | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio dicta autem enim sint sit quasi fugit. Consequatur, nulla aperiam. Consectetur magni molestiae quaerat quas vitae? Eum repellendus earum labore laboriosam.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi esse dignissimos nobis. Iusto modi ducimus temporibus natus voluptate voluptatibus consectetur esse sequi quaerat. Ratione minima nulla enim! Quas, sed facilis.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit, aspernatur. Sed delectus asperiores beatae exercitationem fugit nostrum commodi quis quisquam veniam, debitis quae reiciendis deleniti expedita ullam explicabo enim quibusdam!</p>
+            </div>
+        </>
+     );
+}
+ 
+export default About;
